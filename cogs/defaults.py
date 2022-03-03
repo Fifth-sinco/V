b56cfaebd8b9c8212dd4d6ca727dd434aef59bf9
Fifth-sinco <80825508+Fifth-sinco@users.noreply.github.com>
2022-03-03 21:55:22 +0800

    Update defaults.py

--- cogs/defaults.py
+++ cogs/defaults.py
@@ -13,12 +13,35 @@ class Defaults(commands.Cog):
     @commands.command()
     async def ping(self, ctx):
         """Check for Bot Latency"""
+        await ctx.channel.purge(limit=1)
         await ctx.send(f'Ping = {round(self.client.latency * 1000)}ms')
         
     @commands.command()
     async def clear(self, ctx, amount=5):
         """Clears a set amount of messeges in the specified channel. Default is 5."""
         await ctx.channel.purge(limit=1+amount)
+
+    @commands.command()
+    async def kick(self, ctx, member : discord.Member, *, reason=None):
+        await member.kick(reason=reason)
+
+    @commands.command()
+    async def ban(self, ctx, member : discord.Member, *, reason=None):
+        await member.ban(reason=reason)
+        await ctx.send(f'{member.mention} has beedn banned for {reason}.')
+
+    @commands.command()
+    async def unban(self, ctx, *, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'{user.name}#{user.discriminator} has been unbanned.')
+                return
 
 def setup(client):
     client.add_cog(Defaults(client))
